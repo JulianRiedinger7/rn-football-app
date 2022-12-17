6ee2c9bdc10ec32118c7912b0133237733d572e7
Julian Riedinger <julianriedinger27@gmail.com>
2022-12-17 15:34:54 -0300

    [feature] - initialized firebase

---
+++ src/constants/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDVd_r5JX54tIHrVtzxKqR0x0TAhoyMumo',
+	authDomain: 'rn-football-app.firebaseapp.com',
+	projectId: 'rn-football-app',
+	storageBucket: 'rn-football-app.appspot.com',
+	messagingSenderId: '412369597456',
+	appId: '1:412369597456:web:e8719377059b3e0eb8c695',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

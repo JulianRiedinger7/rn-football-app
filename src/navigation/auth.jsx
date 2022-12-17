7af6a1d0b920c2acbd374c6faf3b430c5075dcc9
Julian Riedinger <julianriedinger27@gmail.com>
2022-12-17 15:18:39 -0300

    [feature] - setup react navigation and created first project scaffolding

---
+++ src/navigation/auth.jsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login, Register } from '../screens';
+
+const Stack = createNativeStackNavigator();
+
+const AuthNavigator = () => {
+	return (
+		<Stack.Navigator initialRouteName="Login">
+			<Stack.Screen name="Login" component={Login} />
+			<Stack.Screen name="Register" component={Register} />
+		</Stack.Navigator>
+	);
+};
+
+export default AuthNavigator;
---
+++ src/navigation/index.jsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './auth';
+
+const AppNavigator = () => {
+	return (
+		<NavigationContainer>
+			<AuthNavigator />
+		</NavigationContainer>
+	);
+};
+
+export default AppNavigator;

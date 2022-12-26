57b8ba04cd86fe5099852db127588460b6524987
Julian Riedinger <julianriedinger27@gmail.com>
2022-12-26 14:18:04 -0300

    [feature] - initialized redux toolkit

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import { Provider } from 'react-redux';
 import AppNavigator from './src/navigation';
+import { store } from './src/store';
 
 export default function App() {
-	return <AppNavigator />;
+	return (
+		<Provider store={store}>
+			<AppNavigator />
+		</Provider>
+	);
 }

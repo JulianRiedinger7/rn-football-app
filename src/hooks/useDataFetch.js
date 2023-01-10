e1899434ed3302bf557d9f8e6fa35ab60a3cfe03
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-01-10 14:29:28 -0300

    [feature] - added dependency to useffect in params

--- src/hooks/useDataFetch.js
+++ src/hooks/useDataFetch.js
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-const useDataFetch = (url) => {
-	const [info, setInfo] = useState([]);
-	const [loading, setLoading] = useState(true);
+const useDataFetch = (url, dep = null) => {
+	const [info, setInfo] = useState([])
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
 		fetch(url)
 			.then((res) => res.json())
 			.then((res) => setInfo(res))
 			.catch((err) => console.error(err))
-			.finally(() => setLoading(false));
-	}, []);
+			.finally(() => setLoading(false))
+	}, [dep])
 
-	return { info, loading };
-};
+	return { info, loading }
+}
 
-export default useDataFetch;
+export default useDataFetch
